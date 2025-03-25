4c9cc963ad44105b8912454890498c60fd4dbe55
RyanHarang <ryh110.rh@gmail.com>
2025-03-24 17:45:02 -0700

    Improving fallback visuals

--- src/components/NowPlaying/NowPlaying.jsx
+++ src/components/NowPlaying/NowPlaying.jsx
@@ -9,11 +9,15 @@ const NowPlaying = () => {
   return (
     <footer className="bg-light-bg2 dark:bg-dark-bg2 flex h-20 w-full items-center justify-between p-4">
       <div className="flex w-1/3 items-center gap-4">
-        <img
-          src={currentTrack?.cover}
-          alt={`Cover of ${currentTrack?.title}`}
-          className="h-18 w-18 overflow-hidden rounded"
-        />
+        {currentTrack ? (
+          <img
+            src={currentTrack?.cover}
+            alt={`Cover of ${currentTrack?.title}`}
+            className="h-18 w-18 overflow-hidden rounded"
+          />
+        ) : (
+          <div className="from-accent via-accent/70 h-18 w-18 overflow-hidden rounded bg-gradient-to-br to-black" />
+        )}
         <div>
           <div className="text-sm font-semibold">
             {currentTrack?.title || "No track playing"}
