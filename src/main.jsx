ce6f5c535095831537128f227e3ba1a19d05a06b
RyanHarang <ryh110.rh@gmail.com>
2025-03-25 17:43:14 -0700

    wrapping with new playlist provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { PlayerProvider } from "./context/PlayerContext.jsx";
+import { PlaylistProvider } from "./context/PlaylistContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <PlayerProvider>
-      <App />
-    </PlayerProvider>
+    <PlaylistProvider>
+      <PlayerProvider>
+        <App />
+      </PlayerProvider>
+    </PlaylistProvider>
   </StrictMode>,
 );
