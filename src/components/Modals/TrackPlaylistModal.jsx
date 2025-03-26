12ef12004e2381576e4e8ba164dfdbed7ee49c10
RyanHarang <ryh110.rh@gmail.com>
2025-03-26 08:18:25 -0700

    Moving modals

--- src/components/Modals/TrackPlaylistModal.jsx
+++ src/components/Modals/TrackPlaylistModal.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { usePlaylists } from "../../context/PlaylistContext.jsx";
 
 const TrackPlaylistModal = ({ track, closeModal }) => {
