ea77984bc5dfe8867a49058bd73dd1d377084cba
RyanHarang <ryh110.rh@gmail.com>
2025-03-27 14:43:32 -0700

    Fixing queue appearance for long titles and adding artists to queue

--- src/components/Queue/Queue.jsx
+++ src/components/Queue/Queue.jsx
@@ -7,15 +7,17 @@ const Queue = () => {
   const { queue, removeFromQueue, clearQueue, setQueue } = usePlayer();
 
   const renderQueueItem = ({ item: track, index, dragHandleProps }) => (
-    <div className="hover:bg-dark-bg3 cursor-grab rounded px-2 py-1 transition active:cursor-grabbing">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <span className="text-dark-fg2" {...dragHandleProps}>
+    <div className="hover:bg-dark-bg3 h-12 cursor-grab overflow-hidden rounded px-2 py-1 transition active:cursor-grabbing">
+      <div className="flex h-full items-center justify-between">
+        <div className="flex w-4/5 items-center gap-2 overflow-hidden">
+          <span className="text-dark-fg2 flex-shrink-0" {...dragHandleProps}>
             <GripIcon />
           </span>
-          <div className="flex flex-col">
-            <span className="p-1 text-sm font-medium">{track.title}</span>
-            <span className="text-xs text-gray-500">{track.artist}</span>
+          <div className="flex flex-grow flex-col overflow-hidden">
+            <span className="truncate text-sm font-medium">{track.title}</span>
+            <span className="truncate text-xs text-gray-500">
+              {track.artists}
+            </span>
           </div>
         </div>
         <TrashButton handleClick={() => removeFromQueue(index)} />
